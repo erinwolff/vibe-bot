bd59af6d6d92ce87841b78065bd9019654732d1a
Erin Wolff <erin_wolff@outlook.com>
2024-05-14 17:30:18 -0700

    increased leave cooldown timer to 10 minutes

--- src/play.js
+++ src/play.js
@@ -2,9 +2,8 @@
 
 module.exports = function playCommand(player) {
   async function execute(interaction) {
-
     // Retrieve the query from options
-    const query = interaction.options.get('query')?.value;
+    const query = interaction.options.get("query")?.value;
 
     // Defer the interaction as things can take time to process
     await interaction.deferReply();
@@ -18,12 +17,14 @@ module.exports = function playCommand(player) {
 
       // Check if the member is in a voice channel
       if (!member || !member.voice.channel) {
-        return interaction.editReply('You are not connected to a voice channel!');
+        return interaction.editReply(
+          "You are not connected to a voice channel!"
+        );
       }
 
       // Check if the query is present before calling player.play
       if (!query) {
-        return interaction.editReply('Please provide a valid query!');
+        return interaction.editReply("Please provide a valid query!");
       }
 
       // Now, proceed with playing the song
@@ -31,21 +32,23 @@ module.exports = function playCommand(player) {
         nodeOptions: {
           metadata: interaction,
           leaveOnEnd: true,
-          leaveOnEndCooldown: 60000,
+          leaveOnEndCooldown: 600000,
           leaveOnEmpty: true,
           leaveOnStop: true,
-          leaveOnStopCooldown: 60000,
+          leaveOnStopCooldown: 600000,
           volume: 6,
         },
       });
 
       // Update the initial reply with the track title
-      return interaction.editReply(`**${track.title}** added to the queue જ⁀➴ `);
+      return interaction.editReply(
+        `**${track.title}** added to the queue જ⁀➴ `
+      );
     } catch (e) {
-      console.error('Error:', e);
+      console.error("Error:", e);
       // Return an error if something failed
       return interaction.editReply(`Something went wrong: ${e}`);
     }
   }
   return execute;
-}
+};
